e89137a4d78544742c501154c1bb10e5ef3b2b70
twopic2 <bowenzhang0@gmail.com>
2026-03-16 16:04:34 -0700

    fixed

--- src/kernel/kernel.main.cpp
+++ src/kernel/kernel.main.cpp
@@ -1,3 +1,9 @@
-void kernel_main() {
 
+extern "C" char __init_array[];
+extern "C" char __init_array_end[];
+
+void kernel_main() {
+    for (auto it = (void(*)())__init_array; it < (void(*)())__init_array_end; it++) {
+        (*it)();
+    }
 }
